ccf0aec832e571eb9f7e91ed013a7ba72d9abf5a
ImFaizanAhmed <imfaizanahmed6@gmail.com>
2024-08-18 16:54:45 +0500

    feat(make hero section responsive): update style to make hero section responsive

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -4,19 +4,18 @@ import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/MovingBorders";
 import { SparklesCore } from "./ui/sparkles";
+import MagicButton from "./MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
 
 function HeroSection() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
-      <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-          Harshil Tomar
+    <div className="h-[30rem] md:h-[40rem] w-full rounded-md flex flex-col items-center relative overflow-hidden mx-auto py-10 md:py-0">
+      {/* <Spotlight className="-top-40 left-0 w-full md:-top-20" fill="white" /> */}
+      <div className="p-4 mt-[100px] lg:mt-[200px] relative z-10 w-full text-center">
+        <h1 className="text-3xl sm:text-6xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Faizan Ahmed
         </h1>
-        <div className="mt-2 w-screen h-40 relative -z-10">
+        <div className="mt-2 w-[cal(100vw - 80px)] h-40 relative -z-10">
           {/* Gradients */}
           {/* <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
@@ -29,42 +28,28 @@ function HeroSection() {
             minSize={0.4}
             maxSize={1}
             particleDensity={1200}
-            className="w-full h-full"
+            className="w-full h-full max-w-[500px] m-auto"
             particleColor="#FFFFFF"
           />
 
           {/* Radial Gradient to prevent sharp edges */}
-          <div className="absolute inset-0 w-full h-full bg-[#000319] [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-          <div className="-mt-4 -ml-8 z-10">
+          <div className="absolute inset-0 w-full max-w-[600px] m-auto h-full bg-[#000319] [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+          <div className="-mt-4 z-10">
             <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-              Software Engineer | FullStack Developer with Passion for Problem
-              Solving
+              <span className="text-purple">3+ Years of Experienced</span>{" "}
+              Semi-Senior Software Engineer
             </p>
             <div className="mt-4">
               <Link href="https://drive.google.com/file/d/1wNJ1J0mZ60GplJ7_qzP0oCkbC4fnz9RV/view">
-                <Button
-                  borderRadius="1.75rem"
-                  className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                >
-                  Chekout Resume
-                </Button>
+                <MagicButton
+                  title="Checkout my work"
+                  icon={<FaLocationArrow />}
+                  position="right"
+                />
               </Link>
             </div>
           </div>
         </div>
-        {/* <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          Software Engineer | FullStack Engineer
-        </p>
-        <div className="mt-4">
-          <Link href={"/courses"}>
-            <Button
-              borderRadius="1.75rem"
-              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-            >
-              Chekout Resume
-            </Button>
-          </Link>
-        </div> */}
       </div>
     </div>
   );
