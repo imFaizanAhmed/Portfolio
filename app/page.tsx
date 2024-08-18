9f296c1084f93296c84f996215d0fd4ff3a5e644
ImFaizanAhmed <imfaizanahmed6@gmail.com>
2024-08-19 01:19:27 +0500

    feat(set spark on large screen): remove max width from main

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ import { Suspense } from "react";
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl">
+      <div className="">
         <Suspense fallback={<div>Loading</div>}>
           <FloatingNav navItems={navItems} />
           {/* <Hero /> */}
